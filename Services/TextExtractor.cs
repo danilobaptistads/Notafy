6eb43a003d2e19c001c1bb31d0f829ceadc9741e
Danilo Baptista <damilobaptistads@gmail.com>
2025-11-24 11:51:47 -0300

    chore: ajuste na classe TextExtract para melhorar performace

--- Services/TextExtractor.cs
+++ Services/TextExtractor.cs
@@ -1,15 +1,24 @@
 using Tesseract;
 namespace Notafy.Services;
 
-class TextExtractor
+class TextExtractor :IDisposable
 {
+    private readonly TesseractEngine engine;
+    public TextExtractor()
+    {
+        engine = new TesseractEngine("./tessdata", "por", EngineMode.Default);
+    }
+
     public string Extract(string imgSrc)
     {
-        using var engine = new TesseractEngine("./tessdata", "por", EngineMode.Default);
-    
         using var img = Pix.LoadFromFile(imgSrc);
         using var page = engine.Process(img);
 
-        return  page.GetText();
+        return page.GetText();
+    }
+
+    public void Dispose()
+    {
+        engine?.Dispose();
     }
 }
